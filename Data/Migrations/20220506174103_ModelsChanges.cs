780623d00784bf46c38b7c5eb3f5bb19de755968
edgar <enyhus@msn.com>
2022-05-07 02:50:45 +0200

    added api/races

---
+++ Data/Migrations/20220506174103_ModelsChanges.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class ModelsChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Signs_SignId",
+                table: "Locations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Waypoints_WaypointId",
+                table: "Locations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Signs_SignId",
+                table: "Locations",
+                column: "SignId",
+                principalTable: "Signs",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Waypoints_WaypointId",
+                table: "Locations",
+                column: "WaypointId",
+                principalTable: "Waypoints",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Signs_SignId",
+                table: "Locations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Waypoints_WaypointId",
+                table: "Locations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Signs_SignId",
+                table: "Locations",
+                column: "SignId",
+                principalTable: "Signs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Waypoints_WaypointId",
+                table: "Locations",
+                column: "WaypointId",
+                principalTable: "Waypoints",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Domain/Contracts/WaypointContract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using GeoAPI.Geometries;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public class WaypointContract
+    {
+        public string? Id { get; set; }
+        public string? Alias { get; set; }
+        public string? Notes { get; set; }
+        public LocationContract Location { get; set; }
+        [JsonPropertyName("race_id")]
+        public string? RaceId { get; set; }
+        //public Race? Race { get; set; }
+    }
+}
